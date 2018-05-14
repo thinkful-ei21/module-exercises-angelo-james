0ff2e5c928c12183148794402e4f07ea7b854bba
chagoy <angelochagoy@gmail.com>
2018-05-14 09:35:22 -0700

    ready to update shopping list

--- scripts/store.js
+++ scripts/store.js
@@ -1,5 +1,5 @@
 const store = (function(){
-  const foo = 'bar';
+
   const items = [
     { id: cuid(), name: 'apples', checked: false },
     { id: cuid(), name: 'oranges', checked: false },
@@ -9,9 +9,51 @@ const store = (function(){
   const hideCheckedItems = false;
   const searchTerm = '';
 
+  const findById = id => {
+    items.find(function(item) {
+      return item.id === id;
+    });
+  };
+
+  const addItem = name => {
+    try {
+      Item.validateName(name);
+      items.push(Item.create(name));
+    } catch (error) {
+      console.error('Cannot add name: ' + error.message);
+    }
+  }
+
+  const findAndToggleChecked = id => {
+    const item = this.findById(id);
+    item.checked = !item.checked;
+  }
+
+  const findAndUpdateName = (id, newName) => {
+    try {
+      Item.validateName(newName);
+      const item = this.findById(id);
+      item.name = newName;
+    } catch (error) {
+      console.error('Cannot update name: ' + error.message);
+    }
+  }
+
+  const findAndDelete = id => {
+    const index = items.findIndex(function(item) {
+      return item.id === id;
+    });
+    items.splice(index, 1);
+  }
+
   return {
     items,
     hideCheckedItems,
     searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
   };
 }());
